bcea0650b491bc996044c0f7a1ffdbb053382310
Veis1337 <117036853+Veis1337@users.noreply.github.com>
2023-04-19 13:21:28 -0400

    file structure

---
+++ README.md
@@ -0,0 +1,27 @@
+# Michael Weiss React Portfolio Page
+
+## Description
+
+This project is a portfolio page for myself to showcase the projects that I have built to potential employers. 
+The motivation behind this project was to blow the HTML/CSS portfolio I built 5 months ago out of the water.
+In this project I was able to implement tailwind over vanilla CSS, use React components to structure the page, and 
+provide functionality I couldn't have dreamed up at the beginning of my coding journey.
+
+## Installation
+
+Deployed at 
+
+## Usage
+
+![](/Assets\Images\Portfolio-screenshot.png)
+
+Open the index.html file in a browser, or navigate to https://veis1337.github.io/Michael-Weiss-Portfolio-Page.html
+The navigation bar in the Header can be used to quickly jump to the different sections of the page.
+
+
+
+## Credits
+
+## License
+
+MIT © Michael Weiss
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import Pkmn from "../assets/projects/PkmnBattler.png";
+import TechBlog from "../assets/projects/TechBlog.png"
+import Wellness from "../assets/projects/WellnessWatcher.png"
+import Mongo from "../assets/mongo.png"
+import Weather from "../assets/projects/weather.png"
+import Music from "../assets/projects/Musiquest.png"
 
 const Work = () => {
   return (
@@ -13,7 +18,7 @@ const Work = () => {
 
           <div 
           style={{ backgroundImage: `url(${Pkmn})` }}
-          className="shadow-lg shadow-[#2b162e] group container rounded-md flex justify-center items-center mx-auto content-div">
+          className="shadow-lg shadow-[#2b162e] bg-[#363b4e] group container rounded-md flex justify-center items-center mx-auto content-div">
 
             {/*Hover Effect*/}
             <div className="opacity-0 group-hover:opacity-100">
@@ -25,7 +30,7 @@ const Work = () => {
               <div className="pt-8 text-center">
                 <a href="/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-[#363b4e]">
-                    Not Deployed Yet 
+                    TBA... 
                   </button>
                 </a>
                 <a href="https://github.com/Veis1337/Pkmn-Battler" target="_blank">
@@ -38,19 +43,19 @@ const Work = () => {
           </div>
             {/*Project 2*/}
           <div 
-          style={{ backgroundImage: `url(${Pkmn})` }}
-          className="shadow-lg shadow-[#2b162e] group container rounded-md flex justify-center items-center mx-auto content-div">
+          style={{ backgroundImage: `url(${TechBlog})` }}
+          className="shadow-lg shadow-[#2b162e] bg-[#363b4e] group container rounded-md flex justify-center items-center mx-auto content-div">
             <div className="opacity-0 group-hover:opacity-100">
               <span className="font-bold px-4 py-3 m-2 text-[#f6e5e5] tracking-wider">
-                MongoDB NoSQL Backend
+                Full Stack MySQL Tech Blog
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://stark-beach-85805.herokuapp.com/" target='_blank'>
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-[#363b4e]">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/Veis1337/tech-blog-db" target='_blank'>
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-[#363b4e]">
                     Code
                   </button>
@@ -60,19 +65,19 @@ const Work = () => {
           </div>
             {/*Project 3*/}
           <div 
-          style={{ backgroundImage: `url(${Pkmn})` }}
-          className="shadow-lg shadow-[#2b162e] group container rounded-md flex justify-center items-center mx-auto content-div">
+          style={{ backgroundImage: `url(${Wellness})` }}
+          className="shadow-lg shadow-[#2b162e] bg-[#363b4e] group container rounded-md flex justify-center items-center mx-auto content-div">
             <div className="opacity-0 group-hover:opacity-100">
               <span className="font-bold px-4 py-3 m-2 text-[#f6e5e5] tracking-wider">
-                MongoDB NoSQL Backend
+                Full Stack MySQL Health App
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://wait-watchers.herokuapp.com/" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-[#363b4e]">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/scottswiger/wait-watchers" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-[#363b4e]">
                     Code
                   </button>
@@ -82,19 +87,19 @@ const Work = () => {
           </div>
             {/*Project 4*/}
           <div 
-          style={{ backgroundImage: `url(${Pkmn})` }}
-          className="shadow-lg shadow-[#2b162e] group container rounded-md flex justify-center items-center mx-auto content-div">
+          style={{ backgroundImage: `url(${Mongo})` }}
+          className="shadow-lg shadow-[#2b162e] bg-[#363b4e] group container rounded-md flex justify-center items-center mx-auto content-div">
             <div className="opacity-0 group-hover:opacity-100">
               <span className="font-bold px-4 py-3 m-2 text-[#f6e5e5] tracking-wider">
                 MongoDB NoSQL Backend
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                {/* <a href=""> */}
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-[#363b4e]">
-                    Demo
+                    No Frontend
                   </button>
-                </a>
-                <a href="/">
+                {/* </a> */}
+                <a href="https://github.com/Veis1337/MongoAPISocialNetwork" target='_blank'>
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-[#363b4e]">
                     Code
                   </button>
@@ -104,19 +109,19 @@ const Work = () => {
           </div>
             {/*Project 5*/}
           <div 
-          style={{ backgroundImage: `url(${Pkmn})` }}
-          className="shadow-lg shadow-[#2b162e] group container rounded-md flex justify-center items-center mx-auto content-div">
+          style={{ backgroundImage: `url(${Music})` }}
+          className="shadow-lg shadow-[#2b162e] bg-[#363b4e] group container rounded-md flex justify-center items-center mx-auto content-div">
             <div className="opacity-0 group-hover:opacity-100">
               <span className="font-bold px-4 py-3 m-2 text-[#f6e5e5] tracking-wider">
-                MongoDB NoSQL Backend
+                JavaScript API App
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://altavada.github.io/musiquest/" target='_blank'>
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-[#363b4e]">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/altavada/musiquest">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-[#363b4e]">
                     Code
                   </button>
@@ -126,19 +131,19 @@ const Work = () => {
           </div>
             {/*Project 6*/}
           <div 
-          style={{ backgroundImage: `url(${Pkmn})` }}
-          className="shadow-lg shadow-[#2b162e] group container rounded-md flex justify-center items-center mx-auto content-div">
+          style={{ backgroundImage: `url(${Weather})` }}
+          className="shadow-lg shadow-[#2b162e] bg-[#363b4e] group container rounded-md flex justify-center items-center mx-auto content-div">
             <div className="opacity-0 group-hover:opacity-100">
               <span className="font-bold px-4 py-3 m-2 text-[#f6e5e5] tracking-wider">
-                MongoDB NoSQL Backend
+                Early JavaScript weather app
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://veis1337.github.io/Weather-Dashboard/" target='_blank'>
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-[#363b4e]">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/Veis1337/Weather-Dashboard" target='_blank'>
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-[#363b4e]">
                     Code
                   </button>
